e3a1a8229bb0aff9d0740d83a095006eff85dbd6
jack <jackfine01@gmail.com>
2025-01-27 07:46:29 -0500

    finishing up for this morning by removing faield deleteTask Method.

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ newProjectDOM.addEventListener('click', function(){
 
 console.log('i live')
 
-const TaskOne = new Task('Title', "Lorem Ipsum", '1/1/1111')
+const TaskOne = new Task('Task One', "Lorem Ipsum", '1/1/1111')
 console.log(TaskOne);
 
-const TaskTwo = new Task('Title', "Lorem Ipsum", '2/2/2222')
+const TaskTwo = new Task('Task Two', "Lorem Ipsum", '2/2/2222')
 console.log(TaskTwo);
 
-const TaskThree = new Task('Title', "Lorem Ipsum", '3/3/3333')
+const TaskThree = new Task('Task Three', "Lorem Ipsum", '3/3/3333')
 console.log(TaskThree);
 
 const ProjectOne = new Project('title,', [])
--- src/project.js
+++ src/project.js
@@ -1,12 +1,20 @@
 class Project { 
-    constructor (title, tasks){
+    constructor (title, tasks = []){
     this._title = title;
     this._tasks = tasks;
     };
 
-    get Title(){
+    get title(){
         return this._title;
     };
+
+    set title(newTitle){
+        this._title = newTitle;
+    };
+
+    get tasks(){
+        return this._tasks;
+    }
 
     addTask( Task ){
         this._tasks.push(Task);
