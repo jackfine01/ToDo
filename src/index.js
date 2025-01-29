aa968c7dbde0f58e620b555bdc830534eb221019
jack <jackfine01@gmail.com>
2025-01-29 07:42:44 -0500

    cleaning up prior tests fron index.js

--- src/index.js
+++ src/index.js
@@ -21,31 +21,3 @@ console.log(ProjectOne.title)
 
 addToProject(TaskFour, projectArray);
 console.log(projectArray);
-
-// console.log('i live')
-
-// const TaskOne = new Task('Task One', "Lorem Ipsum", '1/1/1111')
-// console.log(TaskOne);
-
-// const TaskTwo = new Task('Task Two', "Lorem Ipsum", '2/2/2222')
-// console.log(TaskTwo);
-
-// const TaskThree = new Task('Task Three', "Lorem Ipsum", '3/3/3333')
-// console.log(TaskThree);
-
-// const ProjectOne = new Project('title,', [])
-// console.log(ProjectOne);
-
-// ProjectOne.addTask(TaskOne)
-// ProjectOne.addTask(TaskTwo)
-// ProjectOne.addTask(TaskThree)
-
-// TaskOne.title = "Task One";
-// TaskTwo.date = '03/20/2000';
-// TaskThree.description = 'She Lorem till I ipsum';
-
-// ProjectOne.deleteTask(TaskTwo);
-// console.log(ProjectOne._tasks);
-
-// console.log(generateID)
-// generateID(Math.random());
